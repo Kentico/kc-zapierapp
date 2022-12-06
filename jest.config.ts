6d006b29bc9968f6cd92bdf341b8e6552a4d45f1
Jiri Lojda <jiril@kontent.ai>
2022-12-06 11:47:57 +0100

    Adjust jest config and CI builds
    - use node 16.x on CI as that is the latest version supported by Zapier
    - fix jest test finding

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/src/test/**/*.test.ts']
 };
 
 export default config;
