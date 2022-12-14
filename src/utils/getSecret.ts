d8d9e3329207a547039b441e32a117c0f38dda03
Jiri Lojda <jiril@kontent.ai>
2022-12-14 11:12:37 +0100

    Load key for creating secrets for webhooks from environment variable

--- src/utils/getSecret.ts
+++ src/utils/getSecret.ts
@@ -2,9 +2,13 @@ import { ZObject } from 'zapier-platform-core';
 import * as crypto from 'crypto';
 import { KontentBundle } from '../types/kontentBundle';
 
-const key = 'BP7XqTLDp_9WzvX6BOfsF9mkI25pPD4n4r4FS1d6Fx2pUgXasRDpCOL6tEcnUmVW_MXrc2BRQG1FWu60NHb0tVc';
+const key = process.env['SECRET_KEY'];
 
 export function getSecret(z: ZObject, bundle: KontentBundle<{}>) {
+    if (!key) {
+        throw new Error('Missing environment variable.');
+    }
+
     const zapID = (bundle.authData.projectId ?? '') + (bundle.authData.cmApiKey ?? '') + bundle.authData.previewApiKey;
     return crypto.createHmac('sha256', key).update(zapID, 'utf8').digest('base64');
 }
