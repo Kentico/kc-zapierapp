b7c7974155ad0e7bdb3ded2092cf0b46d2d1ccc4
Jiri Lojda <jiril@kontent.ai>
2022-12-14 11:12:37 +0100

    Load key for creating secrets for webhooks from environment variable

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,8 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/src/test/**/*.test.ts']
+  testMatch: ['**/src/test/**/*.test.ts'],
+  setupFiles: ['./setupTests.ts']
 };
 
 export default config;
