6d2e82398a486128ee6f6bc5f3b751fc9fed8ac2
Ivan Kiráľ <54802833+IvanKiral@users.noreply.github.com>
2023-02-16 10:54:12 +0100

    Remove string formatting and redundant async

--- src/utils/types/getContentTypeSnippets.ts
+++ src/utils/types/getContentTypeSnippets.ts
@@ -2,7 +2,7 @@ import { ZObject } from "zapier-platform-core";
 import { KontentBundle } from "../../types/kontentBundle";
 import { createManagementClient } from "../kontentServices/managementClient";
 
-export const getContentTypeSnippets = async (z: ZObject, bundle: KontentBundle<{}>) =>
+export const getContentTypeSnippets = (z: ZObject, bundle: KontentBundle<{}>) =>
   createManagementClient(z, bundle)
     .listContentTypeSnippets()
     .toPromise()
